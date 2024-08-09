8a5d7c9ef7db224bf60f4f9e99d213d58da1297d
MatthiasBlc <mat.bouloc@gmail.com>
2024-08-09 11:15:39 +0200

    adding hero

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+export const gridItems = [
+  { title: "Title 1", description: "Desc1", id: 1 },
+  { title: "Title 1", description: "Desc1", id: 1 },
+  { title: "Title 1", description: "Desc1", id: 1 },
+  { title: "Title 1", description: "Desc1", id: 1 },
+  { title: "Title 1", description: "Desc1", id: 1 },
+]
